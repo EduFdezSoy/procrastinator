e7ca12388febfad38d63df853128e138c9a528de
Eduardo Fernández <eduardofernandezsoy@gmail.com>
2016-10-15 13:52:33 +0200

    edited ion_auth.php edited ion_auth config and moved mail folder

--- application/config/ion_auth.php
+++ application/config/ion_auth.php
@@ -82,19 +82,19 @@ $config['salt_prefix']    = version_compare(PHP_VERSION, '5.3.7', '<') ? '$2a$' 
  | The controller should check this function and act
  | appropriately. If this variable set to 0, there is no maximum.
  */
-$config['site_title']                 = "Example.com";       // Site Title, example.com
-$config['admin_email']                = "admin@example.com"; // Admin Email, admin@example.com
+$config['site_title']                 = "Procrastinator - Stop Wasting Time";       // Site Title, example.com
+$config['admin_email']                = "procrastinator@edufdezsoy.ovh"; // Admin Email, admin@example.com
 $config['default_group']              = 'members';           // Default group, use name
 $config['admin_group']                = 'admin';             // Default administrators group, use name
 $config['identity']                   = 'email';             // You can use any unique column in your table as identity column. The values in this column, alongside password, will be used for login purposes
 $config['min_password_length']        = 8;                   // Minimum Required Length of Password
 $config['max_password_length']        = 20;                  // Maximum Allowed Length of Password
-$config['email_activation']           = FALSE;               // Email Activation for registration
-$config['manual_activation']          = FALSE;               // Manual Activation for registration
+$config['email_activation']           = TRUE;               // Email Activation for registration
+$config['manual_activation']          = TRUE;               // Manual Activation for registration
 $config['remember_users']             = TRUE;                // Allow users to be remembered and enable auto-login
 $config['user_expire']                = 86500;               // How long to remember the user (seconds). Set to zero for no expiration
-$config['user_extend_on_login']       = FALSE;               // Extend the users cookies every time they auto-login
-$config['track_login_attempts']       = FALSE;               // Track the number of failed login attempts for each user or ip.
+$config['user_extend_on_login']       = TRUE;               // Extend the users cookies every time they auto-login
+$config['track_login_attempts']       = TRUE;               // Track the number of failed login attempts for each user or ip.
 $config['track_login_ip_address']     = TRUE;                // Track login attempts by IP Address, if FALSE will track based on identity. (Default: TRUE)
 $config['maximum_login_attempts']     = 3;                   // The maximum number of failed login attempts.
 $config['lockout_time']               = 600;                 // The number of seconds to lockout an account due to exceeded attempts
@@ -107,8 +107,8 @@ $config['forgot_password_expiration'] = 0;                   // The number of mi
  | remember_cookie_name Default: remember_code
  | identity_cookie_name Default: identity
  */
-$config['remember_cookie_name'] = 'remember_code';
-$config['identity_cookie_name'] = 'identity';
+$config['remember_cookie_name'] = 'pr_remember_me';
+$config['identity_cookie_name'] = 'pr_identity';
 
 /*
  | -------------------------------------------------------------------------
@@ -130,7 +130,7 @@ $config['email_config'] = array(
  | Folder where email templates are stored.
  | Default: auth/
  */
-$config['email_templates'] = 'auth/email/';
+$config['email_templates'] = 'email/';
 
 /*
  | -------------------------------------------------------------------------
