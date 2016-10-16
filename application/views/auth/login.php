2039a9697bc046ea586d9a39ad28d1d1cffe1b72
Eduardo Fernández <eduardofernandezsoy@gmail.com>
2016-10-16 18:28:32 +0200

    Autoload form validation + login form

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,4 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+
 <div class="container">
-    <?php echo $message;?>
+
+    <?php
+    $this->form_validation->set_rules('email', 'Email', 'trim|required');
+    $this->form_validation->set_rules('password', 'Password', 'trim|required');
+    ?>
+
 </div>
