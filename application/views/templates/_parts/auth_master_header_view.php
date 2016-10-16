25a7332b49734e7dae7b53df3bbae37bca8de72f
Eduardo Fernández <eduardofernandezsoy@gmail.com>
2016-10-16 15:15:04 +0200

    Created template files

---
+++ application/views/templates/_parts/auth_master_header_view.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title><?php echo $page_title;?></title>
+    <meta name="description" value="<?php echo $page_description;?>" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <?php echo $before_closing_head;?>
+</head>
+<body>
