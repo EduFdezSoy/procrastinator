aef90ff3f32564c27c9c335265f991c8d70f5c8e
Eduardo Fernández <eduardofernandezsoy@gmail.com>
2016-10-17 22:05:42 +0200

    Added tes encryption_key

--- application/config/config.php
+++ application/config/config.php
@@ -314,7 +314,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'Edu3jDRkfixsRG5ekkU0es1zfmpqo2bI';
 
 /*
 |--------------------------------------------------------------------------
