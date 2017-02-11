43c4c1dc6fe4979d6524b716fb9224ccf81a3032
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-11 21:55:28 +0100

    Changed DB Error page

--- application/views/errors/html/error_db.php
+++ application/views/errors/html/error_db.php
@@ -1,10 +1,38 @@
 <?php
+
+/* 
+ * The MIT License
+ *
+ * Copyright 2016 EduFdezSoy, (Eduardo Fernandez).
+ *
+ * @link https://twitter.com/EduFdezSoy
+ * @mail edufdezsoy@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>Database Error</title>
+<title>Procrastinator - Database Error</title>
 <style type="text/css">
 
 ::selection { background-color: #E13300; color: white; }
@@ -57,8 +85,12 @@ p {
 </head>
 <body>
 	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+        <div class="raton-senala" id="logo">
+            <a href="#" class="nounderline">
+                <div id="titulo-login"><?php echo $heading; ?></div>
+            </a>
+        </div>
+	    <?php echo $message; ?>
 	</div>
 </body>
 </html>
