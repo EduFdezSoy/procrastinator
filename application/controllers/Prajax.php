46f78454b2a324b14454d2afa2ab37861df4b942
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-20 17:22:18 +0100

    removed innecesary info

--- application/controllers/Prajax.php
+++ application/controllers/Prajax.php
@@ -3,21 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Prajax extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/prajax
-	 *	- or -
-	 * 		http://example.com/index.php/prajax/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/prajax/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->model('prajax_model');
