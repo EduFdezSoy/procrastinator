0d371c57e57a46c00823dd826e88944d005d8e88
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-20 18:34:53 +0100

    test 2

--- application/views/main.php
+++ application/views/main.php
@@ -20,6 +20,16 @@ if (window.XMLHttpRequest) {
 }, 2000);
 </script>
 
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
+<script type="text/javascript">
+   var auto_refresh = setInterval(
+      function ()
+      {
+         $('#load_tweets').load('<?=base_url()?>index.php/prajax?f=1').fadeIn("slow");
+      }, 1000); // refresh every 10000 milliseconds
+</script>
+
 <div id="taskslist">
     Loading...
 </div>
+<div id="load_tweets"> aaa</div>
