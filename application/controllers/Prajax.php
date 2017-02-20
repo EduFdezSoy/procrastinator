d4221648b4edb7cd10f3b1327903e699fc8ca2c8
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-20 12:51:42 +0100

    fist try to print something

--- application/controllers/Prajax.php
+++ application/controllers/Prajax.php
@@ -7,19 +7,20 @@ class Prajax extends CI_Controller {
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
+	 * 		http://example.com/index.php/prajax
 	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
+	 * 		http://example.com/index.php/prajax/index
 	 *	- or -
 	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
+	 * map to /index.php/prajax/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
 	{
+		$this->data['tasks'] = $this->prajaxmo->pr_tasks();
 		$this->load->view('welcome_message');
 	}
 }
