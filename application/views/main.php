1ef5f67577e641f7c8c64be9955d33ca1d607d0a
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-20 18:47:39 +0100

    try it now

--- application/views/main.php
+++ application/views/main.php
@@ -2,6 +2,20 @@
 // Copyright (c) 2016 - 2017, Eduardo Fernández
 ?>
 <script>
+if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("taskslist").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","https://proteus.edufdezsoy.es/procrastinator/index.php/prajax?f=1",true);
+        xmlhttp.send();
 setInterval(function(){
 if (window.XMLHttpRequest) {
             // code for IE7+, Firefox, Chrome, Opera, Safari
