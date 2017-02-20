5f5d460b32f4d9c633fc72237c60323165d8ad73
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-20 17:33:14 +0100

    footer, header and a litle main

---
+++ application/views/partials/footer.php
@@ -0,0 +1,16 @@
+        <div class="container-fluid no-selec">
+            <footer id="footer">
+                <p>&copy; 2016 - 2017, Eduardo Fernández</p>
+                <?php if(ENVIRONMENT !== 'production'){ ?>
+                    <p>Rendered in <strong>{elapsed_time}</strong> sec</p>
+                <?php } ?>
+            </footer>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+        <script src="<?=base_url()?>assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="<?=base_url()?>assets/js/main.js" type="text/javascript"></script>
+        <link href="<?=base_url()?>assets/css/datetimepicker.min.css" rel="stylesheet" type="text/css"/>
+        <script src="<?=base_url()?>assets/js/moments.min.js" type="text/javascript"></script>
+        <script src="<?=base_url()?>assets/js/datetimepicker.min.js" type="text/javascript"></script>
+    </body>
+</html>
