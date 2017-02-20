d18ea66a7f17982a67ddb4e6e8874d30d9cf690c
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-20 17:55:17 +0100

    uh

--- application/controllers/Procrastinator.php
+++ application/controllers/Procrastinator.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Procrstinator extends CI_Controller {
+class Procrastinator extends CI_Controller {
 
 	public function index()
 	{
