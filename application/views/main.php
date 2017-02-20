d00b00b9ac55718f61357e0d517f1877546c23f0
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-20 18:30:01 +0100

    added first attempt of ajax

--- application/views/main.php
+++ application/views/main.php
@@ -1,5 +1,25 @@
 <?php
-
-echo "hola";
-
+// Copyright (c) 2016 - 2017, Eduardo Fernández
 ?>
+<script>
+setInterval(function(){
+if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("taskslist").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","<?=base_url()?>index.php/prajax?f=1",true);
+        xmlhttp.send();
+}, 2000);
+</script>
+
+<div id="taskslist">
+    Loading...
+</div>
