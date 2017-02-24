107ce53dfef398d97f24d57bce263f3d8af8d1d8
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-24 10:16:05 +0100

    Comented

--- application/controllers/Prajax.php
+++ application/controllers/Prajax.php
@@ -8,16 +8,18 @@ class Prajax extends CI_Controller {
 		// load model
 		$this->load->model('prajax_model');
 		
-		// get "f" to determinate what to do
+		// get "f" from url to determinate what to do
 		parse_str(substr(strrchr($_SERVER['REQUEST_URI'], "?"), 1), $_GET);
 		$func = $_GET['f'];
 
 		// load task depending of "f"
 		if ($func==1) {
+			// do pr_tasks() from prajax model and store in data['tasks'], then load the view tasks
 			$this->data['tasks'] = $this->prajax_model->pr_tasks();
 			$this->load->view('tasks', $this->data);
 		} elseif ($func==2) {
-			$this->data['completed_tasks'] = $this->prajax_model->pr_tasks();
+			// do pr_completed_tasks() from prajax model and store in data['tasks'], then load the view tasks
+			$this->data['completed_tasks'] = $this->prajax_model->pr_completed_tasks();
 			$this->load->view('tasks', $this->data);
 		}		
 
