875b1d59cbdb347276aacc829be5522a829896b4
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-24 10:38:37 +0100

    commented

--- application/controllers/Procrastinator.php
+++ application/controllers/Procrastinator.php
@@ -3,10 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Procrastinator extends CI_Controller {
 
+	// load index page (page where task are showns)
 	public function index()
 	{
+		// load header (<head>, menu and some css+js)
 		$this->load->view('partials/header');
+		// main, this file only have an AJAX script to load the tasks
         $this->load->view('main');
+		// footer, footer of the page, js and other css that do not require to be loaded at start
         $this->load->view('partials/footer');
 	}
 }
