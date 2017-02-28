9da342b5006766f21a81237f9523147f1a301e8b
Eduardo Fernandez <yo@edufdezsoy.es>
2017-02-28 12:13:41 +0100

    rewriting tasks CSS to SCSS well formated

--- application/views/tasks.php
+++ application/views/tasks.php
@@ -2,6 +2,8 @@
 // Copyright (c) 2016 - 2017, Eduardo Fernández
  ?>
 
+<!--
+
 <div class="container">
     
     <?php foreach ($tasks as $task) { ?>
@@ -29,3 +31,34 @@
     <?php } ?>
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
+
+-->
+
+<!-- Rewriting tasks -->
+
+    <?php foreach ($tasks as $task) { ?>
+        <!-- Task container -->
+        <div class="panel panel-default well" id="<?=$task->id?>">
+            <!-- Top, task -->
+            <div class="panel-body">
+                <span class="glyphicon glyphicon-certificate" style="font-size: 25px; vertical-align: middle; padding-right: 5px; color: #<?=$task->colour?>;"></span>
+                <span style="vertical-align: middle;"><?=$task->task?></span>
+            </div>
+            <!-- Bottom, task date, button.. -->
+            <div class="panel-footer raton-normal">
+                <div class="row">
+                    <div class="col-xs-9 col-md-10">
+                        Por <span title="<?=$task->first_name?> <?=$task->last_name?>"><b><?=$task->username?></b></span> el <?=$task->date?>
+                    </div>
+                    <div class="col-xs-2 hidden-sm hidden-md hidden-lg" style="margin-left: -25px;">
+                        <button class="btn btn-xs btn-success">Complete</button>
+                    </div>
+                    <div class="hidden-xs col-sm-2">
+                        <button class="btn btn-success">Complete</button>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- Task container -->
+        
+    <?php } ?>
+    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
