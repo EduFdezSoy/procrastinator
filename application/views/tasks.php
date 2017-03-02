49a95d33b758b08074429274e0b953b8cbe0c391
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-02 19:11:42 +0100

    this will no longer be usefull

--- application/views/tasks.php
+++ application/views/tasks.php
@@ -26,4 +26,3 @@
         </div> <!-- Task container -->
         
     <?php } ?>
-    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
