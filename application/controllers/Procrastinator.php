4bf491292f701aa83950816491d07b5fcfeed809
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-02 17:16:49 +0100

    added tabs view and passes data

--- application/controllers/Procrastinator.php
+++ application/controllers/Procrastinator.php
@@ -6,8 +6,16 @@ class Procrastinator extends CI_Controller {
 	// load index page (page where task are showns)
 	public function index()
 	{
+		// load model
+		$this->load->model('pr_model');
+
+		// catch tabs from the model and asign it to data['tabs']
+		$this->data['tabs'] = $this->pr_model->pr_tabs();
+
 		// load header (<head>, menu and some css+js)
 		$this->load->view('partials/header');
+		// load the tabs for the tasks
+		$this->load->view('tabs', $this->data);
 		// main, this file only have an AJAX script to load the tasks
         $this->load->view('main');
 		// footer, footer of the page, js and other css that do not require to be loaded at start
