9b6c5b3822cd7a7330524764e30cf135dbce790f
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-02 18:49:56 +0100

    added script to change .active class

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,34 +5,53 @@
  *
  */
 
-// script que oculta los elementos de id mensaje a los 3 segundos
-setTimeout("document.getElementById(\"mensaje\").style.display = \"none\"",3000);
+// change .active clas from tabs
+var selector, elems, makeActive;
 
-// este script solo te deja meter numeros en un imput donde metas onkeypress="return valida(event)"
-function writenum(e){
+selector = '#tabs li';
+
+elems = document.querySelectorAll(selector);
+
+makeActive = function() {
+    for (var i = 0; i < elems.length; i++)
+        elems[i].classList.remove('active');
+
+    this.classList.add('active');
+};
+
+for (var i = 0; i < elems.length; i++)
+    elems[i].addEventListener('mousedown', makeActive);
+
+
+
+// script que oculta los elementos de id mensaje a los 3 segundos
+setTimeout("document.getElementById(\"mensaje\").style.display = \"none\"", 3000);
+
+// este script solo te deja meter numeros en un input donde metas onkeypress="return valida(event)"
+function writenum(e) {
     tecla = (document.all) ? e.keyCode : e.which;
     //Tecla de retroceso para borrar, siempre la permite
-    if (tecla==8){
+    if (tecla == 8) {
         return true;
     }
     // Patron de entrada, en este caso solo acepta numeros
-    patron =/[]/;
+    patron = /[]/;
     tecla_final = String.fromCharCode(tecla);
     return patron.test(tecla_final);
 }
 // script para mostrar menu de selecion de fecha y hora
-$(function () {
-                $('#datetimepicker1').datetimepicker({
-                    format: 'DD/MM/YYYY H:mm',
-                    locale: 'es'
-                });
-            });
-$(function () {
-                $('#datetimepicker2').datetimepicker({
-                    format: 'DD/MM/YYYY',
-                    locale: 'es'
-                });
-            });
+$(function() {
+    $('#datetimepicker1').datetimepicker({
+        format: 'DD/MM/YYYY H:mm',
+        locale: 'es'
+    });
+});
+$(function() {
+    $('#datetimepicker2').datetimepicker({
+        format: 'DD/MM/YYYY',
+        locale: 'es'
+    });
+});
 
 // script contador (hacia atrás) de minutos y segundos (ids JScuentaMin y JScuentaSeg) AUN NO FUNCIONA CORRECTAMENTE
 /* $tiempom = document.getElementById("JScuentaMin").innerHTML;
@@ -67,7 +86,7 @@ function coge_id(element) {
     // todo esto es na mas que para el color
     var color_element = document.getElementById(tarea_id).getElementsByClassName("col-xs-2")[0];
     style = window.getComputedStyle(color_element),
-    tarea_color = style.getPropertyValue('background-color');
+        tarea_color = style.getPropertyValue('background-color');
     // coge el id de la categoria y la recorta para que sea solo el numero (id de la categoria)
     var categoria = document.getElementsByName(tarea_id)[0].parentNode.parentNode.parentNode.parentNode.id;
     var categoria_id = categoria.substr(1);
@@ -75,5 +94,5 @@ function coge_id(element) {
     document.getElementById("JScoge_id").setAttribute("value", tarea_id);
     document.getElementById("JScoge_tarea").innerHTML = tarea;
     document.getElementById("JScoge_color").style.backgroundColor = tarea_color;
-    document.getElementById("JScoge_cat").children[0].children[categoria_id-1].setAttribute("selected", "selected");
+    document.getElementById("JScoge_cat").children[0].children[categoria_id - 1].setAttribute("selected", "selected");
 }
