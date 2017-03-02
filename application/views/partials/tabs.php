046b0fc18be6529e2c0a37323fd6bedcba9b62d1
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-02 18:19:39 +0100

    Tabs.php and css

--- application/views/partials/tabs.php
+++ application/views/partials/tabs.php
@@ -2,20 +2,12 @@
 // Copyright (c) 2016 - 2017, Eduardo Fernández
  ?>
 
-<ul class="nav nav-tabs">
+<ul id="tabs">
 
     <?php foreach ($tabs as $tab) { ?>
     
-        <li class="active"><a href="#"><?=$tab->name?></a></li>
-        <li><a href="#">Menu 1</a></li>
-        <li><a href="#">Menu 2</a></li>
-        <li><a href="#">Menu 3</a></li>
-    
-        <!-- Task container -->
-        <div class="task-container" id="<?=$task->id?>">
-
+        <li id="Tab-<?=$tab->id?>"><a href="#"><?=$tab->name?></a></li>
         
     <?php } ?>
 
 </ul>
-<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
