1caf42fa99d8063fc25c746a10e37fd2ad9a5a91
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-02 18:36:05 +0100

    test wh classes

--- application/views/partials/tabs.php
+++ application/views/partials/tabs.php
@@ -1,13 +1,12 @@
 <?php
 // Copyright (c) 2016 - 2017, Eduardo Fernández
  ?>
+    <ul id="tabs" class="col-md-offset-2 col-md-8">
 
-<ul id="tabs">
-
-    <?php foreach ($tabs as $tab) { ?>
-    
-        <li <? if($tab->order==1){echo 'class="active"';}?> id="Tab-<?=$tab->id?>"><a href="#"><?=$tab->name?></a></li>
+        <?php foreach ($tabs as $tab) { ?>
         
-    <?php } ?>
+            <li <? if($tab->order==1){echo 'class="active"';}?> id="Tab-<?=$tab->id?>"><a href="#"><?=$tab->name?></a></li>
+            
+        <?php } ?>
 
-</ul>
+    </ul>
