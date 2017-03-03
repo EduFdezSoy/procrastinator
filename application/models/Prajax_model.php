a86e9ddaa8fcafc63aa5bdc47aba69814741266a
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-03 17:48:00 +0100

    test

--- application/models/Prajax_model.php
+++ application/models/Prajax_model.php
@@ -24,7 +24,7 @@ class Prajax_model extends CI_Model
            $this->db->select('tasks.id, tasks.task, tasks.date, tasks.colour, tasks.tab_id, u2.username, u2.first_name, u2.last_name, tasks.priority');
            $this->db->from('tasks');
            $this->db->join('users u2', 'tasks.user_id = u2.id', 'left');
-           $this->db->where('tasks.status = 0 and tasks.tab_id = $tab');
+           $this->db->where('tasks.status = 0 and tasks.tab_id = 1');
            $sql = $this->db->get();
            $result = $sql->result();
            return $result;
