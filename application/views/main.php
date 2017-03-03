4a99d70a58b2b094e296c4ee409469950e6a8f53
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-03 17:29:49 +0100

    js for tabs

--- application/views/main.php
+++ application/views/main.php
@@ -48,21 +48,21 @@ if (window.XMLHttpRequest) {
 var tab = 1;
 // Ajax request function
 function ajaxCall(){
-var num = window.tab;
-if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5 (someone still using them?)
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    var num = window.tab;
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5 (someone still using them?)
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("taskslist").innerHTML = this.responseText;
         }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("taskslist").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET","https://proteus.edufdezsoy.es/procrastinator/index.php/prajax?f="+num,true);
-        xmlhttp.send();
+    };
+    xmlhttp.open("GET","https://proteus.edufdezsoy.es/procrastinator/index.php/prajax?f="+num,true);
+    xmlhttp.send();
 }
 // Ajax call
  ajaxCall();
