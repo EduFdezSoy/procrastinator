8522aa2cc482ab8032d7c9682982bea207882a48
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-03 15:07:38 +0100

    js edit for tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,8 +24,25 @@ for (var i = 0; i < elems.length; i++)
 
 
 // change a var value for AJAX call
-function tabnum(n) {
+function tabNum(n) {
 
+    // Define tab to the ajax call
+    var tab = n;
+    // First AJAX request on page load
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("taskslist").innerHTML = this.responseText;
+        }
+    };
+    xmlhttp.open("GET", "https://proteus.edufdezsoy.es/procrastinator/index.php/prajax?f=" + tab, true);
+    xmlhttp.send();
 }
 
 
