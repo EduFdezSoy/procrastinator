2dfcec254f50c6b15fac7d05a2859610930a4864
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-03 17:33:27 +0100

    removed old js code

--- application/views/main.php
+++ application/views/main.php
@@ -2,47 +2,6 @@
 // Copyright (c) 2016 - 2017, Eduardo Fernández
 ?>
 
-<!--
-
-<script>
-// Define tab to the ajax call
-var tab = 1;
-// First AJAX request on page load
-if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("taskslist").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET","https://proteus.edufdezsoy.es/procrastinator/index.php/prajax?f="+tab,true);
-        xmlhttp.send();
-// Periodic Ajax requests
-setInterval(function(){
-if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5 (someone still using them?)
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("taskslist").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET","https://proteus.edufdezsoy.es/procrastinator/index.php/prajax?f="+tab,true);
-        xmlhttp.send();
-}, 10000); // Time: 10s
-</script>
-
- -->
-
 <script>
 // Define tab to the ajax call
 var tab = 1;
