707eb0d369d03139c0a4af0c8f032067a9d32311
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-05 15:21:47 +0100

    modal firs edit

--- application/views/main.php
+++ application/views/main.php
@@ -40,17 +40,17 @@ function ajaxCall(){
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Create task</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        something to send here
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">Add task</button>
       </div>
     </div>
   </div>
