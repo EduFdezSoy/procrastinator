739870fb882d1d4b7d5a6aaeab99f18c163ca88d
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-05 15:23:02 +0100

    fixed

--- application/views/main.php
+++ application/views/main.php
@@ -40,7 +40,7 @@ function ajaxCall(){
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Create task</h5>
+        <h5 class="modal-title" id="addTasksModal">Create task</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
