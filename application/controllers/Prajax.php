7bc65ee1063962fec8ff34131820911f93284a6d
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-07 14:02:19 +0100

    FIxed a little problem

--- application/controllers/Prajax.php
+++ application/controllers/Prajax.php
@@ -10,24 +10,34 @@ class Prajax extends CI_Controller {
 		
 		// get "f" from url to determinate what to do
 		parse_str(substr(strrchr($_SERVER['REQUEST_URI'], "?"), 1), $_GET);
-		$func = $_GET['f'];
+		$tab = $_GET['f'];
 
-		// check get value is an int
-		if( (! filter_var($func, FILTER_VALIDATE_INT)) OR (! $func === 0) ) {
+		// check if it is 0
+		if ($tab == 0) {
+
+			// do pr_completed_tasks() from prajax model and store in data['tasks']
+			$this->data['tasks'] 		= $this->prajax_model->pr_completed_tasks();
+			$this->data['dateformat'] 	= 'j \o\f F, Y';
+
+			// load the view tasks
+			$this->load->view('tasks', $this->data);
+
+		// check if get value is an INT
+		} elseif ( ! filter_var($tab, FILTER_VALIDATE_INT) ) {
+
+			// if its not an INT redirect to "what are you trying?"
 			redirect('/procrastinator/whatareyoutrying');
-		}
 
-		// load task depending of "f"
-		if ($func>=1) {
-			// do pr_tasks() from prajax model and store in data['tasks'], then load the view tasks
-			$this->data['tasks'] = $this->prajax_model->pr_tasks($func);
-			$this->data['dateformat'] = 'l, j \o\f F';
-			$this->load->view('tasks', $this->data);
-		} elseif ($func==0) {
-			// do pr_completed_tasks() from prajax model and store in data['tasks'], then load the view tasks
-			$this->data['tasks'] = $this->prajax_model->pr_completed_tasks();
-			$this->data['dateformat'] = 'j \o\f F, Y';
-			$this->load->view('tasks', $this->data);
+		// if it is an INT, finally, we do this
+		} else {
+
+		// do pr_tasks() from prajax model and store in data['tasks']
+		$this->data['tasks'] 		= $this->prajax_model->pr_tasks($tab);
+		$this->data['dateformat'] 	= 'l, j \o\f F';
+
+		// load the view tasks
+		$this->load->view('tasks', $this->data);
+
 		}
 	}
 }
