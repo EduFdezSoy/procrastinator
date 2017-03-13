199603cb99a2023cea3cf6d29d8063ca759f1fe0
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-13 18:35:37 +0100

    added undo() function

--- application/controllers/Dowithtask.php
+++ application/controllers/Dowithtask.php
@@ -18,7 +18,7 @@ class Dowithtask extends CI_Controller {
 		{
 			redirect('auth/login');
 		}
-		
+
 		// loads the model we are going to use
 		$this->load->model('dowithtask_model');
 
@@ -28,7 +28,7 @@ class Dowithtask extends CI_Controller {
 		$new_task_tab 	= $this->input->post('tab');
 		// we need to check the user, will do it tomorrow
 		$new_task_user 	= "1";
-		
+
 		// we may send it in the order of the db
 		$this->dowithtask_model->add_task(
 			$new_task_user,
@@ -36,14 +36,14 @@ class Dowithtask extends CI_Controller {
 			$new_task_tab,
 			$new_task_color
 		);
-		
+
 		// show OK message
 		echo 'OK';
 	}
 
 	public function complete()
 	{
-		
+
 		// check if user is login in
 		if (!$this->ion_auth->logged_in())
 		{
@@ -63,13 +63,46 @@ class Dowithtask extends CI_Controller {
 
 		// we need to check the user, will do it tomorrow
 		$user = "1";
-		
+
 		// we may send it in the order of the db
 		$this->dowithtask_model->complete_task(
 			$task_id,
 			$user
 		);
-		
+
+		// now, redirect to main page
+		redirect('/procrastinator');
+	}
+
+	public function undo()
+	{
+
+		// check if user is login in
+		if (!$this->ion_auth->logged_in())
+		{
+			redirect('auth/login');
+		}
+
+		// loads the model we are going to use
+		$this->load->model('dowithtask_model');
+
+		// get "task_id" from url to determinate what to do
+		$task_id = $this->uri->segment(3);
+
+		// check get value is an int
+		if( ! filter_var($task_id, FILTER_VALIDATE_INT) ){
+			redirect('/procrastinator/whatareyoutrying');
+		}
+
+		// we need to check the user, will do it tomorrow
+		$user = "1";
+
+		// we may send it in the order of the db
+		$this->dowithtask_model->complete_task(
+			$task_id,
+			$user
+		);
+
 		// now, redirect to main page
 		redirect('/procrastinator');
 	}
