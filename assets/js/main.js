11f730210999c44dc403cef347cd32c3afc63704
Eduardo Fernandez <yo@edufdezsoy.es>
2017-03-24 10:29:45 +0100

    do not use php in js file, remember

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ function completeTask(n) {
             console.log("Complete task: OK");
         }
     };
-    xmlhttp.open("GET", "<?=base_url()?>dowithtask/complete/" + tid, true);
+    xmlhttp.open("GET", "/dowithtask/complete/" + tid, true);
     xmlhttp.send();
 
     // Update task list in time and restart auto-updater
