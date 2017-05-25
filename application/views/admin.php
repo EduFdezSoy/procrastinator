36c5f3aadedf232952f24ca537e210e3545fed6c
Eduardo Fernandez <yo@edufdezsoy.es>
2017-05-25 14:17:46 +0200

    starting page

---
+++ application/views/admin.php
@@ -0,0 +1,15 @@
+<?php
+// Copyright (c) 2016 - 2017, Eduardo Fernández
+?>
+
+<div class="container-fluid">
+    <div class="container" id="last-log-attempts">
+    </div>
+    <div class="container" id="users">
+    lista de usuarios
+    </div>
+    <div class="container" id="user-admin">
+    controles de usuario, añadir, (des)bloquear, eliminar...
+    </div>
+
+</div>
